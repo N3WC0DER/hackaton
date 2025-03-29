cc702688557b736ea783bb5e585fd097e0ab4cc3
N3WC0DER <sawkin.nik@gmail.com>
2025-03-29 09:32:54 +0000

    send first message

--- voice-receiver/main.py
+++ voice-receiver/main.py
@@ -1,3 +1,5 @@
+import datetime
+import time
 import json
 from kafka import KafkaProducer
 import os
@@ -5,13 +7,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
 KAFKA_TOPIC = 'stt-queue'
 
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+def get_kafka_producer():
+    retries = 5
+    while retries > 0:
+        try:
+            return KafkaProducer(
+                bootstrap_servers=os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092').split(','),
+                value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+            )
+        except Exception as e:
+            print(f"Failed to connect to Kafka: {e}, retrying...")
+            retries -= 1
+            time.sleep(10)
+    raise Exception("Could not connect to Kafka after retries")
+
+# Инициализация после проверки зависимостей
+producer = get_kafka_producer()
 
 def send_to_kafka():
     """Отправка сообщения в Kafka"""
@@ -25,7 +38,6 @@ def send_to_kafka():
 def main():
     """Основной цикл"""
     while True:
-        print(f"Starting call processing at {datetime.now()}")
         print("Processing completed. Waiting for 1 hours...")
         send_to_kafka()
         time.sleep(60 * 60) # раз в час
